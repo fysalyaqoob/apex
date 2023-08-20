585f024bf9d73623d73279b570bf6ae4056d9f94
RoyalFlushever <royalflushdiamond918@gmail.com>
2023-08-21 01:54:19 +0500

    gulp reset

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,96 @@
+## v1.0.14
+...
+## v1.0.13
+
+8e43b70 remove scss from dist
+08737e0 ver
+efaf6c4 gulp
+f1169f0 gulp
+61eec89 gulp
+67f210f updated
+0ba5a72 git release
+c3070c2 git release
+b482a5b Create npm-gulp.yml
+e7e586d mega menu
+5e53cd8 mega menu
+d0ad99c homepage
+1d2aba0 Initial commit
+
+## v1.0.12
+
+
+
+## v1.0.11
+
+8e43b70 remove scss from dist
+08737e0 ver
+efaf6c4 gulp
+f1169f0 gulp
+61eec89 gulp
+67f210f updated
+0ba5a72 git release
+c3070c2 git release
+b482a5b Create npm-gulp.yml
+e7e586d mega menu
+5e53cd8 mega menu
+d0ad99c homepage
+1d2aba0 Initial commit
+
+## v1.0.10
+
+8e43b70 remove scss from dist
+08737e0 ver
+efaf6c4 gulp
+f1169f0 gulp
+61eec89 gulp
+67f210f updated
+0ba5a72 git release
+c3070c2 git release
+b482a5b Create npm-gulp.yml
+e7e586d mega menu
+5e53cd8 mega menu
+d0ad99c homepage
+1d2aba0 Initial commit
+
+## v1.0.9
+
+8e43b70 remove scss from dist
+08737e0 ver
+efaf6c4 gulp
+f1169f0 gulp
+61eec89 gulp
+67f210f updated
+0ba5a72 git release
+c3070c2 git release
+b482a5b Create npm-gulp.yml
+e7e586d mega menu
+5e53cd8 mega menu
+d0ad99c homepage
+1d2aba0 Initial commit
+
+## v1.0.8
+
+8e43b70 remove scss from dist
+08737e0 ver
+efaf6c4 gulp
+f1169f0 gulp
+61eec89 gulp
+67f210f updated
+0ba5a72 git release
+c3070c2 git release
+b482a5b Create npm-gulp.yml
+e7e586d mega menu
+5e53cd8 mega menu
+d0ad99c homepage
+1d2aba0 Initial commit
+
+## v1.0.5
+
+8e43b70 remove scss from dist
+08737e0 ver
+efaf6c4 gulp
+f1169f0 gulp
+
 ## v1.0.5
 
 efaf6c4 gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,256 +1,141 @@
 "use strict";
 
-var gulp = require('gulp'),
+const gulp = require('gulp'),
   sass = require('gulp-sass')(require('sass')),
   del = require('del'),
-  uglify = require('gulp-uglify'),
   cleanCSS = require('gulp-clean-css'),
   rename = require("gulp-rename"),
-  merge = require('merge-stream'),
   htmlreplace = require('gulp-html-replace'),
   autoprefixer = require('gulp-autoprefixer'),
-  purgecss = require('gulp-purgecss'),
   fileinclude = require('gulp-file-include'),
   htmlbeautify = require('gulp-html-beautify'),
-  zip = require('gulp-zip'),
-  ghRelease = require('gh-release'),
+  webpack = require('webpack-stream'),
+  TerserPlugin = require('terser-webpack-plugin'),
   browserSync = require('browser-sync').create();
 
-  require('dotenv').config();
+// Clean up 'dist' directory
+gulp.task('clean', () => del(['dist']));
 
-// Clean task
-gulp.task('clean', function() {
-  return del(['dist', 'assets/css/app.css']);
-});
+// Copy bootstrap SCSS files
+gulp.task('bootstrap:scss', () => gulp.src(['./node_modules/bootstrap/scss/**/*'])
+  .pipe(gulp.dest('./assets/scss/bootstrap')));
 
-// Copy third party libraries from node_modules into /vendor
-gulp.task('vendor:js', function() {
-  return gulp.src([
-    './node_modules/bootstrap/dist/js/*',
-    './node_modules/@popperjs/core/dist/umd/popper.*'
-  ])
-    .pipe(gulp.dest('./assets/js/vendor'));
-});
-
-// Copy bootstrap-icons from node_modules into /fonts
-gulp.task('vendor:fonts', function() {
-  return  gulp.src([
-    './node_modules/bootstrap-icons/**/*',
-    '!./node_modules/bootstrap-icons/package.json',
-    '!./node_modules/bootstrap-icons/README.md',
-  ])
-    .pipe(gulp.dest('./assets/fonts/bootstrap-icons'))
-});
-
-// vendor task
-gulp.task('vendor', gulp.parallel('vendor:fonts', 'vendor:js'));
-
-// Copy vendor's js to /dist
-gulp.task('vendor:build', function() {
-  var jsStream = gulp.src([
-    './assets/js/vendor/bootstrap.bundle.min.js',
-    './assets/js/vendor/popper.min.js'
-  ])
-    .pipe(gulp.dest('./dist/assets/js/vendor'));
-  var fontStream = gulp.src(['./assets/fonts/bootstrap-icons/**/*.*']).pipe(gulp.dest('./dist/assets/fonts/bootstrap-icons'));
-  return merge (jsStream, fontStream);
-})
-
-// Copy Bootstrap SCSS(SASS) from node_modules to /assets/scss/bootstrap
-gulp.task('bootstrap:scss', function() {
-  return gulp.src(['./node_modules/bootstrap/scss/**/*'])
-    .pipe(gulp.dest('./assets/scss/bootstrap'));
-});
-
-// Compile SCSS(SASS) files
+// Compile and Minify SCSS to .min.css
 gulp.task('scss', gulp.series('bootstrap:scss', function compileScss() {
   return gulp.src(['./assets/scss/*.scss'])
-    .pipe(sass.sync({
-      outputStyle: 'expanded',
-      quietDeps: true
-    }).on('error', sass.logError))
+    .pipe(sass.sync({ outputStyle: 'expanded', quietDeps: true }).on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(gulp.dest('./assets/css'))
-}));
-
-// Minify CSS
-gulp.task('css:minify', gulp.series('scss', function cssMinify() {
-  return gulp.src("./assets/css/*.css")
     .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./dist/assets/css'))
     .pipe(browserSync.stream());
 }));
 
-// Minify Js
-gulp.task('js:minify', function () {
-  return gulp.src([
-    './assets/js/app.js'
-  ])
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+// Copy SCSS files without minification
+gulp.task('scss:copy', gulp.series('bootstrap:scss', function copyScss() {
+  return gulp.src(['./assets/scss/*.scss'])
+    .pipe(sass.sync({ outputStyle: 'expanded', quietDeps: true }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./dist/assets/css'))
+    .pipe(browserSync.stream());
+}));
+
+const webpackConfig = {
+  mode: 'production',
+  entry: {
+    app: './assets/js/app.js' // Specify the entry point for webpack
+  },
+  output: {
+    filename: '[name].min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+};
+
+// JS: Webpack (minified)
+gulp.task('js:webpack:minified', () => {
+  return gulp.src('./assets/js/app.js')
+    .pipe(webpack(webpackConfig))
     .pipe(gulp.dest('./dist/assets/js'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('html:partials', function() {
-  return gulp.src(['*.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(browserSync.stream());
-});
-
-// Replace HTML block for Js and Css file to min version upon build and copy to /dist
-gulp.task('replaceHtmlBlock', function () {
-  return gulp.src(['dist/*.html']) // Target dist directory
-    .pipe(htmlreplace({
-      'js': 'assets/js/app.min.js',
-      'css': 'assets/css/app.min.css'
-    }))
-    .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.stream());
-});
-
-// Configure the browserSync task and watch file path for change
-gulp.task('dev', function browserDev(done) {
-  browserSync.init({
-    server: {
-      baseDir: "./dist"
-    }
-  });
-  gulp.watch(['assets/scss/*.scss', 'assets/scss/**/*.scss', '!assets/scss/bootstrap/**'], gulp.series('css:minify', function cssBrowserReload(done) {
-    browserSync.reload();
-    done(); //Async callback for completion.
-  }));
-  gulp.watch('assets/js/app.js', gulp.series('js:minify', function jsBrowserReload(done) {
-    browserSync.reload();
-    done();
-  }));
-
-  // Watch for changes in HTML files and process them
-  gulp.watch(['*.html', './partials/**/*.html'], gulp.series('html:partials', function htmlBrowserReload(done) {
-    browserSync.reload();
-    done();
-  }));
-
-  done();
-});
-
-gulp.task("copyAssets", function() {
-  return gulp.src([
-    '!*.html',
-    "assets/**/*",
-    "!assets/scss/**",  // This line excludes all SCSS files
-  ], { base: './'})
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('purge-unused-css', function() {
-  return gulp.src('./dist/assets/css/*.min.css') 
-    .pipe(purgecss({
-      content: ['./dist/*.html', './dist/assets/js/*.js'], 
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    }))
-    .pipe(gulp.dest('./dist/assets/css')); 
-});
-
-gulp.task('beautify-html', function() {
-  var options = {
-    indentSize: 2
+// JS: Webpack (unminified)
+gulp.task('js:webpack:unminified', () => {
+  const unminifiedWebpackConfig = {
+    ...webpackConfig,
+    mode: 'development',
+    optimization: {
+      minimize: false,
+    },
   };
+
+  return gulp.src('./assets/js/app.js')
+    .pipe(webpack(unminifiedWebpackConfig))
+    .pipe(rename({ suffix: '.unmin' }))
+    .pipe(gulp.dest('./dist/assets/js'))
+    .pipe(browserSync.stream());
+});
+
+// Process HTML partials
+gulp.task('html:partials', () => gulp.src(['*.html'])
+  .pipe(fileinclude({ prefix: '@@', basepath: '@file' }))
+  .pipe(htmlreplace({ 'js': 'assets/js/app.min.js', 'css': 'assets/css/app.min.css' }))
+  .pipe(gulp.dest('./dist/'))
+  .pipe(browserSync.stream())
+);
+
+// Copy assets to 'dist' directory excluding SCSS and app.js
+gulp.task("copyAssets", () => gulp.src(['!*.html', "assets/**/*", "!assets/scss/**", "!assets/js/app.js"], { base: './' })
+  .pipe(gulp.dest('dist'))
+);
+
+// Beautify HTML files
+gulp.task('beautify-html', () => {
+  const options = { indentSize: 2 };
   return gulp.src('dist/*.html')
     .pipe(htmlbeautify(options))
     .pipe(gulp.dest('dist/'))
 });
 
-// This task creates a zip archive from the 'dist' directory
-gulp.task('zip', function() {
-  return gulp.src('dist/**/*')
-    .pipe(zip('release.zip'))
-    .pipe(gulp.dest('release'));
-});
+// Main build task
+gulp.task("build", gulp.series("clean", gulp.parallel('scss', 'js:webpack:minified', /*'js:webpack:unminified',*/ 'scss:copy'), 'html:partials', 'copyAssets', 'beautify-html'));
 
-const fs = require('fs');
+// Development task with file watcher
+gulp.task('dev', gulp.series('build', function watchChanges(done) {
+  browserSync.init({ server: { baseDir: "./dist" } });
 
-function getNextVersion(currentVersion) {
-    let [major, minor, patch] = currentVersion.slice(1).split('.').map(Number); // slice to remove 'v' prefix
-    patch++;
-    return `v${major}.${minor}.${patch}`;
-}
+  // Watch SCSS files and run the SCSS task on changes
+  gulp.watch(['assets/scss/*.scss', 'assets/scss/**/*.scss', '!assets/scss/bootstrap/**'], gulp.series('scss'));
 
-// This can be a gulp task
-gulp.task('bump-version', function(done) {
-    const currentVersion = 'v1.0.4'; // This could be read from a file or retrieved from the last GitHub tag.
-    const nextVersion = getNextVersion(currentVersion);
-    
-    // Now you'd update wherever you store the current version. For example:
-    fs.writeFileSync('VERSION.txt', nextVersion);
-    done();
-});
+  // Process JS changes with Webpack (both minified and unminified)
+  gulp.watch('assets/js/**/*.js', gulp.parallel('js:webpack:minified', /*'js:webpack:unminified'*/));
 
-const { execSync } = require('child_process');
+  // Process HTML on changes
+  gulp.watch(['*.html', './partials/**/*.html'], gulp.series('html:partials'));
 
-gulp.task('update-changelog', function(done) {
-    const lastVersion = 'v1.0.4'; // This could be read from a file or retrieved from the last GitHub tag.
-    const nextVersion = getNextVersion(lastVersion);
-    
-    // Get all commit messages since last version
-    const commits = execSync(`git log ${lastVersion}..HEAD --oneline`).toString().trim();
-    
-    // Append to changelog
-    const changelog = fs.readFileSync('CHANGELOG.md', 'utf8');
-    fs.writeFileSync('CHANGELOG.md', `## ${nextVersion}\n\n${commits}\n\n${changelog}`);
-    
-    done();
-});
+  done();
+}));
 
-const releaseNotes = '...';
-
-// This task creates a new GitHub release with the zipped file
-gulp.task('github:release', function(done) {
-  const nextVersion = getNextVersion('v1.0.4'); // The version can be read from a file.
-
-  ghRelease({
-      repo: 'apex',
-      auth: {
-          token: process.env.GITHUB_TOKEN
-      },
-      tag: nextVersion, // Use the new version
-      notes: 'Release notes for this version.', // This can be read from the updated CHANGELOG.md file
-      assets: ['release/release.zip'],
-      body: releaseNotes // Corrected this line
-  }, function(err, result) {
-    if (err) {
-      console.error(err);
-      done(err);
-      return;
-    }
-    console.log('Release was successful! Check it on GitHub.');
-    done();
-  });
-});
-
-
-gulp.task('release', gulp.series('bump-version', 'update-changelog', 'zip', 'github:release'));
-
-// Build task
-gulp.task("build", gulp.series(
-  "clean",
-  gulp.parallel('css:minify', 'js:minify', 'vendor'),
-  'html:partials',
-  'copyAssets',
-  'beautify-html',
-  'replaceHtmlBlock',
-  'purge-unused-css',
-  'vendor:build'
-));
-
-// Default task
+// Default task - runs the build task
 gulp.task("default", gulp.series("build"));
