cbb1637845b2dd2d8a26bd257ad1880ca8ac2569
RoyalFlushever <royalflushdiamond918@gmail.com>
2023-08-22 21:20:04 +0500

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
-import 'bootstrap';
+// Import Bootstrap and assign its Toast component to a constant
+import * as Bootstrap from 'bootstrap';
+const Toast = Bootstrap.Toast;
+
+// Rest of your JavaScript code
+const toastTrigger = document.getElementById('liveToastBtn');
+const toastLiveExample = document.getElementById('liveToast');
+
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new Toast(toastLiveExample);
+    toast.show();
+  });
+}
 
 import './custom';
--- dist/assets/js/custom.js
+++ dist/assets/js/custom.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // Check if the parallax container is in the viewport
             if (scrollPosition > containerTop - window.innerHeight && scrollPosition < containerTop + parallaxContainers[index].offsetHeight) {
                 // Calculate the parallax offset based on how much of the container is visible
-                const parallaxOffset = (scrollPosition - containerTop) * 0.3;
+                const parallaxOffset = (scrollPosition - containerTop) * 0.5;
 
                 // Update the position of the current parallax background
                 background.style.transform = `translateY(${parallaxOffset}px)`;
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', handleParallax);
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Check if the site-header and floating window elements exist on the page
     let siteHeader = document.querySelector('.site-header');
     let floatingWindow = document.querySelector('#liveToast');
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Adjust position on window resize to make it responsive
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', function () {
         if (siteHeader && floatingWindow) {
             siteHeaderHeight = siteHeader.offsetHeight;
             topPosition = siteHeaderHeight + 35;
@@ -54,4 +54,55 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    adjustSubmenuPosition();
+    window.addEventListener('resize', adjustSubmenuPosition);
+});
 
+function adjustSubmenuPosition() {
+    // Assuming menuItems are direct children of the navbar or another container
+    const menuItems = document.querySelectorAll('#primary-nav .dropdown');
+
+    menuItems.forEach(menuItem => {
+        menuItem.addEventListener('mouseover', function () {
+            const submenu = this.querySelector('.dropdown-menu, #primary-nav ul ul');
+            
+            if (submenu && !submenu.dataset.positionChecked) {
+                const rightEdge = submenu.getBoundingClientRect().right;
+                const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+                if (rightEdge > viewportWidth) {
+                    submenu.classList.add('has-submenu-right');
+                }
+                
+                // Add a class or data attribute to remember that we've checked this submenu's position
+                submenu.dataset.positionChecked = 'true';
+            }
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carousel = document.querySelector('#apxCarousel');
+    const carouselInner = carousel.querySelector('.carousel-inner');
+    const carouselItems = carouselInner.querySelectorAll('.carousel-item');
+    const carouselIndicators = carousel.querySelector('.carousel-indicators');
+
+    // Clear existing indicators
+    carouselIndicators.innerHTML = '';
+
+    carouselItems.forEach((item, index) => {
+        const indicator = document.createElement('button');
+        indicator.type = 'button';
+        indicator.dataset.bsTarget = '#apxCarousel';
+        indicator.dataset.bsSlideTo = index;
+        indicator.setAttribute('aria-label', `Slide ${index + 1}`);
+
+        if (index === 0) {
+            indicator.classList.add('active');
+            indicator.setAttribute('aria-current', 'true');
+        }
+
+        carouselIndicators.appendChild(indicator);
+    });
+});
